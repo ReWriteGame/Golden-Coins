1f514f20eaa991a71e6f096c00c463975bc6de29
ReWriteGame <mr.ekstrimal1432@gmail.com>
2021-09-21 23:48:48 +0300

    end Timer Script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,46 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private float timeBetweenCallsChangedTimeEvent = 1;
-    [SerializeField] private bool startOnAwake = true;
-    [SerializeField] private bool directionMin = true;
-
+    [SerializeField] private float currentTime = 0;
     [SerializeField] private float minTime = 0;
     [SerializeField] private float maxTime = 10;
-    [SerializeField] private float currentTime = 0;
+
+    //[SerializeField] private float timeBetweenCallsChangedTimeEvent = 1;
+    [SerializeField] private bool startOnAwake = true;
+    [SerializeField] private bool countdown = true;
 
     public UnityEvent startTimeEvent;
-    public UnityEvent pauseTimeEvent;
     public UnityEvent stopTimeEvent;
     public UnityEvent isMinTimeEvent;
     public UnityEvent isMaxTimeEvent;
-    public UnityEvent changedTimeEvent;
+    //public UnityEvent pauseTimeEvent;
+    //public UnityEvent changedTimeEvent;
+
 
     private Coroutine activeTimerLinkCor = null;
+    private float startTime;
+
+    public float CurrentTime { get => currentTime; private set => currentTime = value; }
+    public float MinTime { get => minTime; private set => minTime = value; }
+    public float MaxTime { get => maxTime; private set => maxTime = value; }
+    public float StartTime { get => startTime; private set => startTime = value; }
+
 
     private void Start()
     {
-        if(startOnAwake) StartTime();
+        startTime = currentTime;
+        if (startOnAwake) StartTimer();
     }
 
-    public void StartTime()
+    public void StartTimer()
     {
-        StopTime();
+        StopTimer();
         activeTimerLinkCor = StartCoroutine(TimerCor());
+        startTimeEvent?.Invoke();
     }
 
-    public void StopTime()
+    public void StopTimer()
     {
-        StopCoroutine(activeTimerLinkCor);
-    }
-
-    public void PauseTime()
-    {
-
+        if(activeTimerLinkCor != null)
+            StopCoroutine(activeTimerLinkCor);
+        stopTimeEvent?.Invoke();
     }
 
     public void AddTime(float value)
@@ -57,12 +64,12 @@ public class Timer : MonoBehaviour
         TimeIsMin();
     }
 
-
     public bool TimeIsMin()
     {
         if (currentTime == minTime) isMinTimeEvent?.Invoke();
         return currentTime == minTime;
     }
+
     public bool TimeIsMax()
     {
         if (currentTime == maxTime) isMaxTimeEvent?.Invoke();
@@ -73,23 +80,18 @@ public class Timer : MonoBehaviour
     {
         while (true)
         {
-            if (directionMin)
-            {
-               currentTime = currentTime <= maxTime ? (currentTime + Time.deltaTime) : maxTime;
-               if (TimeIsMax()) yield break;
-            }
-            else
+            if (countdown)
             {
                 currentTime = currentTime >= minTime ? (currentTime - Time.deltaTime) : minTime;
                 if (TimeIsMin()) yield break;
+            }
+            else
+            {
+                currentTime = currentTime <= maxTime ? (currentTime + Time.deltaTime) : maxTime;
+                if (TimeIsMax()) yield break;
             }
             yield return null;// pause on 1 frame
         }
         yield break;
     }
-
-
-    
-
-
 }
