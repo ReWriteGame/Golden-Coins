2d8108d51edc45118dc0420a1b5a36344423bde1
nout <mr.ekstrimal1432@gmaii.com>
2021-09-22 14:27:09 +0300

    add music

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Shield : MonoBehaviour
 {
+    public UnityEvent knifeHitEvent;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Knife>())
+        {
             collision.gameObject.GetComponent<Destroyable>().destroy();
+            knifeHitEvent?.Invoke();
+        }
+
     }
 }
